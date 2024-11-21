3cec3ccf7aadd8a2c2184aa3e36ecd4dd1f4e093
lse-7660 <lse76609251@gmail.com>
2024-11-21 15:30:18 +0900

    목업데이터 만들기

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,6 +3,27 @@ import TodoHd from './TodoHd';
 import TodoEditor from './TodoEditor';
 import TodoList from './TodoList';
 
+const mockupTodoData = [
+    {
+        id: 1,
+        isDone: false,
+        task: '수업 복습하기',
+        createdDate: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        task: '밥먹기',
+        createdDate: new Date().getTime(),
+    },
+    {
+        id: 3,
+        isDone: false,
+        task: '러커 그리기',
+        createdDate: new Date().getTime(),
+    },
+];
+
 const Todo = () => {
     return (
         <div>
